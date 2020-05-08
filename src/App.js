483b32a226b2b0676063383bbc4e75ac108b9f7a
Lucas Mota <lucas.o.mota@gmail.com>
2020-05-07 23:18:12 -0300

    Solução do desafio completa

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import api from "../src/services/api";
 import "./styles.css";
 
-const projects = [
-  { title: "1", descricao: "dasdas" },
-  { title: "1", descricao: "dasdas" },
-];
-
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  useEffect(() => {
+    api.get("/repositories").then((response) => {
+      setRepositories(...repositories, response.data);
+    });
+  }, []);
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("/repositories", {
+      title: `Novo Projeto ${Math.floor(Math.random() * 40)}`,
+      url: "http://lucas.mota.com.br",
+      techs: ["nodeJs", "Mongo"],
+    });
+    const newRepo = response.data;
+    setRepositories([...repositories, newRepo]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      const response = await api.delete(`/repositories/${id}`);
+      if (response.status === 204) {
+        setRepositories(repositories.filter((repo) => repo.id !== id));
+      }
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        {projects.map((project) => (
-          <li>
-            {project.title}
-            <button onClick={() => handleRemoveRepository(1)}>Remover</button>
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
           </li>
         ))}
       </ul>
